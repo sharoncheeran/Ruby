bbcd1f064a9a0ff58ef14c2eaec2583366691c96
Sharon Cheeran <sharon.cheeran@superdry.com>
2023-07-21 10:39:46 +0100

    CRUD working and new model created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,40 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
+      flash[:success] = "Object was successfully created"
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        flash[:success] = "Object was successfully updated"
+        redirect_to @article
+      else
+        flash[:error] = "Something went wrong"
+        render :new, status: :unprocessable_entity
+      end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to root_path, status: :see_other
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to articles_url
+    end
+  end
+  
 
   private
     def article_params
